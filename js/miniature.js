6847d155c7fc94980f237f7536a9a879db63c39f
Anna <lilimp8@gmail.com>
2024-03-16 23:36:40 +0600

    Добавляет открытие фото

--- js/miniature.js
+++ js/miniature.js
@@ -1,4 +1,5 @@
-import { generatePhotos } from './generate-photos.js';
+import {openPicture} from './big-picture.js';
+import {generatePhotos} from './generate-photos.js';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictureListElement = document.querySelector('.pictures');
@@ -8,13 +9,20 @@ const photos = generatePhotos();
 const pictureListFragment = document.createDocumentFragment();
 
 const initMiniature = () => {
-  photos.forEach(({url, description, likes, comments}) => {
+  photos.forEach((photo) => {
+    const {url, description, likes, comments} = photo;
     const pictureElement = pictureTemplate.cloneNode(true);
 
     pictureElement.querySelector('.picture__img').src = url;
     pictureElement.querySelector('.picture__img').alt = description;
     pictureElement.querySelector('.picture__likes').textContent = likes;
     pictureElement.querySelector('.picture__comments').textContent = comments.length;
+
+    pictureElement.addEventListener('click', (evt) => {
+      if (evt.target.closest('.picture')) {
+        openPicture(photo);
+      }
+    });
 
     pictureListFragment.appendChild(pictureElement);
   });
