9cdc2b80c84094147850290e2b4db10553117a35
Anna <lilimp8@gmail.com>
2024-04-04 13:03:11 +0600

    Подготавливает проект

--- js/sorting-picture.js
+++ js/sorting-picture.js
@@ -8,6 +8,7 @@ const sortRandomElement = formSortingElement.querySelector('#filter-random');
 const sortDiscussedElement = formSortingElement.querySelector('#filter-discussed');
 
 let currentSortElement = sortDefaultElement;
+
 const RANDOM_PICTURE_COUNT = 10;
 
 const randomSorting = (pictures) => {
@@ -23,30 +24,32 @@ const randomSorting = (pictures) => {
 
 const compareCommentsLength = (pictureA, pictureB) => pictureB.comments.length - pictureA.comments.length;
 
+const sortMiniature = (pictures) => {
+  switch (currentSortElement){
+    case sortDefaultElement:
+      clearMiniature();
+      initMiniature(pictures);
+      break;
+    case sortRandomElement:
+      clearMiniature();
+      initMiniature(randomSorting(pictures));
+      break;
+    case sortDiscussedElement:
+      clearMiniature();
+      initMiniature(pictures.slice().sort(compareCommentsLength));
+  }
+};
 
 const initSortingPictures = (pictures) => {
-  sortingElement.classList.remove('img-filters--inactive');
   initMiniature(pictures);
+  sortingElement.classList.remove('img-filters--inactive');
 
-  formSortingElement.addEventListener('click', debounce((evt) => {
+  formSortingElement.addEventListener('click', (evt) => {
     currentSortElement.classList.remove('img-filters__button--active');
     currentSortElement = evt.target;
     currentSortElement.classList.add('img-filters__button--active');
-
-    switch (currentSortElement){
-      case sortDefaultElement:
-        clearMiniature();
-        initMiniature(pictures);
-        break;
-      case sortRandomElement:
-        clearMiniature();
-        initMiniature(randomSorting(pictures));
-        break;
-      case sortDiscussedElement:
-        clearMiniature();
-        initMiniature(pictures.slice().sort(compareCommentsLength));
-    }
-  }));
+    debounce(sortMiniature(pictures));
+  });
 };
 
 export {initSortingPictures};
